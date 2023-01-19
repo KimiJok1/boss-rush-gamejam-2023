d3bd024d6e19196e0274c4a72562c1667d00e4d3
Joe-vis <joel.visakorpi@edu.tampere.fi>
2023-01-20 00:01:03 +0200

    updated healthbars

--- Project/Assets/Scripts/EnemyController.cs
+++ Project/Assets/Scripts/EnemyController.cs
@@ -7,32 +7,53 @@ using TMPro;
 public class EnemyController : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField]private float speed = 6.0f;
+    [SerializeField] private float speed = 6.0f;
     private float horizontalInput;
 
     [Header("Jumping")]
-    [SerializeField]private float jumpForce = 4.0f;
-    [SerializeField]private float gravityModifier = 1.0f;
+    [SerializeField] private float jumpForce = 4.0f;
+    [SerializeField] private float gravityModifier = 1.0f;
     private bool isOnGround = false;
 
     [Header("Sprites")]
-    [SerializeField]private GameObject sprite;
-    private GameObject canvas;
+    [SerializeField] private GameObject sprite;
+    [SerializeField] private Slider[] healthBar;
+    [SerializeField] private TextMeshProUGUI healthText;
 
     [Header("Stats")]
     private int health = 100;
-    [SerializeField]private int maxHealth = 100;
+    private int previousHealth = 100;
+    [SerializeField] private int maxHealth = 100;
+
+    private float LastHitTime = 0.0f;
 
     void Start()
     {
         // gravity
         Physics.gravity *= gravityModifier;
-        canvas = GameObject.Find("Canvas");
         health = maxHealth;
+        previousHealth = health;
+
+        foreach (Slider slider in healthBar)
+        {
+            slider.maxValue = maxHealth;
+            slider.value = maxHealth;
+        }
     }
 
     void Update()
     {
+        if(LastHitTime > 0.0f)
+        {
+            LastHitTime -= Time.deltaTime;
+        }
+        else
+        {
+            if(previousHealth > health)
+            {
+                healthBar[1].value -= .1f;
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -43,15 +64,18 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    public void UpdateUI()
+    private void UpdateUI()
     {
-        canvas.transform.Find("Healthbar").GetComponent<Slider>().value = health;
-        canvas.transform.Find("HealthText").GetComponent<TextMeshProUGUI>().text = health.ToString() + "/" + maxHealth.ToString();
+
+        healthBar[0].value = health;
+        healthText.text = health.ToString() + "/" + maxHealth.ToString();
     }
 
     public void OnHit()
     {
+        previousHealth = health;
         health -= 10;
+        LastHitTime = 1.0f;
 
         if (health <= 0)
             Die();
