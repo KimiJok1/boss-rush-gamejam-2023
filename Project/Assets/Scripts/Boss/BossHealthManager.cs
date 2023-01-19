e8516b92f403f1385d3313d61b6fc213dae906c7
Joe-vis <joel.visakorpi@edu.tampere.fi>
2023-01-20 01:12:27 +0200

    boss health bar

--- Project/Assets/Scripts/Boss/BossHealthManager.cs
+++ Project/Assets/Scripts/Boss/BossHealthManager.cs
@@ -5,16 +5,49 @@ using UnityEngine.UI;
 
 public class BossHealthManager : MonoBehaviour
 {
-    [SerializeField] private Slider healthBar;
+    [SerializeField] private Slider[] healthBar;
 
-    void SetMaxHealth(int health)
+    private float LastHitTime = 0.0f;
+    private int previousHealth = 100;
+    private int health = 100;
+
+    void Start()
     {
-        healthBar.maxValue = health;
-        healthBar.value = health;
+        SetMaxHealth(100);
+        SetHealth(80);
     }
 
-    void SetHealth(int health)
+    void Update()
     {
-        healthBar.value = health;
+        if( LastHitTime > 0.0f )
+        {
+            LastHitTime -= Time.deltaTime;
+        }
+        else
+        {
+            if(previousHealth > health)
+            {
+                healthBar[1].value -= .1f;
+            }
+        }
+    }
+
+    void SetMaxHealth(int _health)
+    {
+        previousHealth = _health;
+        health = _health;
+        foreach(Slider slider in healthBar)
+        {
+            slider.maxValue = _health;
+            slider.value = _health;
+        }
+    }
+
+    void SetHealth(int _health)
+    {
+        LastHitTime = 1.0f;
+        previousHealth = health;
+        health = _health;
+        healthBar[0].value = _health;
     }
 }
